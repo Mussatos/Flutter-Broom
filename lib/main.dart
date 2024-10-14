7aded1eda8e8f031cec3d6c03aa4e580de650119
Antonio <146999516+Mussatos@users.noreply.github.com>
2024-10-14 19:51:01 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:broom_main_vscode/Login.dart';
 import 'package:broom_main_vscode/user_provider.dart';
-import 'package:broom_main_vscode/user_view.dart';
 import 'package:broom_main_vscode/view/user_list.dart';
 import 'package:flutter/material.dart';
 import 'package:broom_main_vscode/signup.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
-  await dotenv.load(fileName: '.env');
   runApp(UserProvider(
       child: MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -17,12 +14,25 @@ void main() async {
     home: HomePage(),
     routes: {
       "/list": (_) => UserList(),
-      //"view": (_) => UserView(),
     },
   )));
 }
 
 class HomePage extends StatelessWidget {
+
+  controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..setNavigationDelegate(
+    NavigationDelegate(
+      onProgress: (int progress) {
+        // Update loading bar.
+      },
+      onPageStarted: (String url) {},
+      onPageFinished: (String url) {},
+    ),
+  )
+  ..loadFlutterAsset('assets/adsense.html');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +48,7 @@ class HomePage extends StatelessWidget {
               children: <Widget>[
                 Column(
                   children: <Widget>[
+                    WebViewWidget(controller: controller),
                     Text(
                       "Bem vindo!",
                       style: TextStyle(

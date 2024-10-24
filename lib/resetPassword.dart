c386cdcf29250ecd161a10fe2973c5ad634e5d46
LianMiranda <lianmiranda26@gmail.com>
2024-10-24 15:41:15 -0400

    STYLE: Adicioonando stilo para tela de redefinir senha

--- lib/resetPassword.dart
+++ lib/resetPassword.dart
@@ -14,17 +14,16 @@ class ResetPasswordScreen extends StatefulWidget {
 class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController =
-      TextEditingController();
-
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  
   bool _isLoading = false;
+  String? passwordError;
+  String? confirmPasswordError;
 
   Future<void> _resetPassword() async {
     if (_formKey.currentState!.validate()) {
-      // Usando o token passado como argumento
       String token = widget.token ?? '';
 
-      print("token"+token);
       if (token.isEmpty) {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           content: Text('Token inválido ou ausente.'),
@@ -53,13 +52,13 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
             content: Text('Senha alterada com sucesso!'),
           ));
         } else {
-          print("Erro: ${response.body}"); // Log de erro
+          print("Erro: ${response.body}"); 
           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
             content: Text('Falha ao alterar a senha. Tente novamente.'),
           ));
         }
       } catch (e) {
-        print("Erro na requisição: $e"); // Log de erro
+        print("Erro na requisição: $e"); 
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           content: Text('Erro na conexão. Tente novamente.'),
         ));
@@ -74,49 +73,109 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Redefinir Senha'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+      backgroundColor: Color(0xFF2ECC8F),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Nova Senha'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira a nova senha';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _confirmPasswordController,
-                decoration: InputDecoration(labelText: 'Confirmar Senha'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, confirme a nova senha';
-                  }
-                  if (value != _passwordController.text) {
-                    return 'As senhas não correspondem';
-                  }
-                  return null;
-                },
+              Text(
+                "Redefina sua senha",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
               ),
               SizedBox(height: 20),
-              _isLoading
-                  ? CircularProgressIndicator()
-                  : ElevatedButton(
-                      onPressed: _resetPassword,
-                      child: Text('Redefinir Senha'),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    _buildPasswordField(
+                      label: 'Nova Senha',
+                      controller: _passwordController,
+                      errorText: passwordError,
+                      onChanged: (value) {
+                        setState(() {
+                          passwordError = value.isEmpty ? 'Insira uma senha' : null;
+                        });
+                      },
                     ),
+                    SizedBox(height: 10),
+                    _buildPasswordField(
+                      label: 'Confirmar Senha',
+                      controller: _confirmPasswordController,
+                      errorText: confirmPasswordError,
+                      onChanged: (value) {
+                        setState(() {
+                          confirmPasswordError = value != _passwordController.text
+                              ? 'As senhas não correspondem'
+                              : null;
+                        });
+                      },
+                    ),
+                                       SizedBox(height: 20),
+                    _isLoading
+                        ? CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.white), // Cor do carregador
+                          )
+                        : SizedBox(
+                            width: 350,
+                            height: 50,
+                            child: ElevatedButton(
+                              onPressed: _resetPassword,
+                              child: Text('Redefinir Senha'),
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(Colors.black),
+                                foregroundColor: MaterialStateProperty.all(Colors.white),
+                              ),
+                            ),
+                          ),
+
+                  ],
+                ),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPasswordField({
+    required String label,
+    required TextEditingController controller,
+    String? errorText,
+    required Function(String) onChanged,
+  }) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      child: SizedBox(
+        width: 350,
+        child: TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            labelText: label,
+            errorText: errorText,
+            labelStyle: TextStyle(color: Colors.white),
+            filled: true,
+            fillColor: Colors.transparent,
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red),
+            ),
+          ),
+          style: TextStyle(color: Colors.white),
+          obscureText: true,
+          onChanged: onChanged,
         ),
       ),
     );

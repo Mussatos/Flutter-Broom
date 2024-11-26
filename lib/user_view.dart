868073c7f507a0a204e8faf3b08db1fdbbb38b65
ViniTkL <vinimaxter150@gmail.com>
2024-11-26 14:31:48 -0400

    [FIX] removendo a não visualização

--- lib/user_view.dart
+++ lib/user_view.dart
@@ -49,7 +49,8 @@ class UserView extends StatelessWidget {
         ]),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator(color: Color(0xFF2ECC8F)));
+            return const Center(
+                child: CircularProgressIndicator(color: Color(0xFF2ECC8F)));
           } else if (snapshot.hasError) {
             return const Center(child: Text('Erro ao carregar usuários'));
           } else if (!snapshot.hasData) {
@@ -87,18 +88,16 @@ class UserView extends StatelessWidget {
                       textAlign: TextAlign.center,
                     ),
                     SizedBox(height: 10),
-                    if (profileId == 1) ...[
-                      Text(
-                        'Serviço: ${userData.wantService ? "Está à procura." : "Não está necessitando."}',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey.shade700,
-                        ),
-                        textAlign: TextAlign.center,
+                    Text(
+                      'Serviço: ${userData.wantService ? "Está à procura." : "Não está necessitando."}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey.shade700,
                       ),
-                      SizedBox(height: 10),
-                    ],
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10),
                     Text(
                       formatAddress,
                       style: TextStyle(
